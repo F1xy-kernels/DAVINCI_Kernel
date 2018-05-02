548e104ee56cb70cd866785d95d43c7c11888572
Sami Tolvanen <samitolvanen@google.com>
2018-05-02 10:44:59 -0700

    FROMLIST: add support for Clang's Shadow Call Stack (SCS)
    This change adds generic support for Clang's Shadow Call Stack,
    which uses a shadow stack to protect return addresses from being
    overwritten by an attacker. Details are available here:
    
      https://clang.llvm.org/docs/ShadowCallStack.html
    
    Note that security guarantees in the kernel differ from the
    ones documented for user space. The kernel must store addresses
    of shadow stacks used by other tasks and interrupt handlers in
    memory, which means an attacker capable reading and writing
    arbitrary memory may be able to locate them and hijack control
    flow by modifying shadow stacks that are not currently in use.
    
    Bug: 145210207
    Change-Id: Ia5f1650593fa95da4efcf86f84830a20989f161c
    (am from https://lore.kernel.org/patchwork/patch/1149054/)
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -95,6 +95,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
+#include <linux/scs.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 

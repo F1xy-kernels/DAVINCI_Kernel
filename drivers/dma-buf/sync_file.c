8dda3eb7b88d65908b634aa719244fd8fa1714cd
Park Ju Hyung <qkrwngud825@gmail.com>
2019-07-09 18:03:21 +0900

    dma-buf: Avoid dynamic memory allocation for small info buffers
    Most info buffers are rather small and can fit on the stack,
    eliminating the need to allocate them dynamically. Reserve a 240B
    stack buffer for this purpose to avoid the overhead of dynamic
    memory allocation.
    
    Co-authored-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/dma-buf/sync_file.c
+++ drivers/dma-buf/sync_file.c
@@ -375,6 +375,7 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 {
 	struct sync_file_info info;
 	struct sync_fence_info *fence_info = NULL;
+	struct sync_fence_info fence_info_onstack[4] __aligned(8);
 	struct dma_fence **fences;
 	__u32 size;
 	int num_fences, ret, i;
@@ -409,9 +410,15 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 
 	offset = offsetof(typeof(*fence_info), status);
 	size = sizeof(*fence_info) - offset;
-	fence_info = kzalloc(size * num_fences, GFP_KERNEL);
-	if (!fence_info)
-		return -ENOMEM;
+
+	if (likely(size <= sizeof(fence_info_onstack))) {
+		memset(&fence_info_onstack, 0, sizeof(fence_info_onstack));
+		fence_info = fence_info_onstack;
+	} else {
+		fence_info = kzalloc(size * num_fences, GFP_KERNEL);
+		if (!fence_info)
+			return -ENOMEM;
+	}
 
 	for (i = 0; i < num_fences; i++) {
 		struct sync_fence_info *fence = (void *)&fence_info[i] - offset;
@@ -435,7 +442,8 @@ no_fences:
 		ret = 0;
 
 out:
-	kfree(fence_info);
+	if (unlikely(fence_info != fence_info_onstack))
+		kfree(fence_info);
 
 	return ret;
 }

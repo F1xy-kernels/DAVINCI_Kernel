72e7fd9a4e3469c1ec1c35588079c92d714c5af6
Yaroslav Furman <yaro330@gmail.com>
2020-01-09 20:33:18 +0300

    Revert "sched: Add task boost feature"
    We don't use it anyway.
    
    This reverts commit e85a065760e76a218c143ee7474b7dba8c1caeff.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7450,7 +7450,8 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (capacity == max_capacity)
 		return true;
 
-	if (task_boost_policy(p) == SCHED_BOOST_ON_BIG &&
+	if ((task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
+			schedtune_task_boost(p) > 10) &&
 			is_min_capacity_cpu(cpu))
 		return false;
 
@@ -8271,8 +8272,8 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	int placement_boost = task_boost_policy(p);
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
-	int boosted = (schedtune_task_boost(p) > 0 && p->prio <= DEFAULT_PRIO)
-							|| per_task_boost(p) > 0;
+	int boosted = (schedtune_task_boost(p) > 0 &&
+				   p->prio <= DEFAULT_PRIO);
 	bool about_to_idle = (cpu_rq(cpu)->nr_running < 2);
 
 	fbt_env.fastpath = 0;

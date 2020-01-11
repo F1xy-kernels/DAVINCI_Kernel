36df4be4a4f84ac86406f7bae53a05a87236c640
Yaroslav Furman <yaro330@gmail.com>
2020-01-11 22:01:01 +0300

    fs: add a simplified file blocker
    fs/exec.c part was not needed.
    
    [from prev version]:
    Unlike previous versions of this monstrocity, this version
    doesn't hide anything, it just makes files un-executable.
    
    file_blocker.h contains two arrays, paths and files.
    If a path from blocked list is detected check_file() function
    blocks access to files that contain string from blocked files list
    in their path.
    
    Inspired by:
    the PUBG blocker [1] from Diab Neiroukh <officiallazerl0rd@gmail.com>
    And commit [2] from Demon000 <demonsingur@gmail.com>
    https://github.com/RaphielGang/spins_kernel_xiaomi_sdm845/commit/75804821e68be3ece
    https://github.com/Demon000/kernel_xiaomi_sm8150/commit/cf076061980b95ce
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- fs/open.c
+++ fs/open.c
@@ -1079,48 +1079,31 @@ EXPORT_SYMBOL(filp_clone_open);
 
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 static char *files_array[] = {
-	"com.feravolt",
 	"fde",
 	"lspeed",
 	"nfsinjector",
 	"lkt",
-	"MAGNE",
 };
 
 static char *paths_array[] = {
 	"/data/adb/modules",
-	"/system/etc",
-	"/data/app"
 };
-
-static bool string_compare(const char *arg1, const char *arg2)
-{
-	return !strncmp(arg1, arg2, strlen(arg2));
-}
 
 static bool inline check_file(const char *name)
 {
 	int i, f;
 	for (f = 0; f < ARRAY_SIZE(paths_array); ++f) {
-		char *path_to_check = paths_array[f];
+		const char *path_to_check = paths_array[f];
 
-		if (unlikely(string_compare(name, path_to_check))) {
+		if (!strncmp(name, path_to_check, strlen(path_to_check))) {
 			for (i = 0; i < ARRAY_SIZE(files_array); ++i) {
 				const char *filename = name + strlen(path_to_check) + 1;
-				char *filename_to_check = files_array[i];
+				const char *filename_to_check = files_array[i];
 
-				/* Leave only the actual filename */
-				if (string_compare(filename, filename_to_check)) {
-					pr_info_ratelimited("%s: blocking %s\n", __func__, name);
+				/* Leave only the actual filename for strstr check */
+				if (strstr(filename, filename_to_check)) {
+					pr_info("%s: blocking %s\n", __func__, filename);
 					return 1;
-				} else if (string_compare(name, "/data/app")) {
-					const char *filename_doublecheck = strchr(filename, '/');
-					if (filename_doublecheck == NULL)
-						return 0;
-					if (string_compare(filename_doublecheck + 1, filename_to_check)) {
-						pr_info_ratelimited("%s: blocking %s\n", __func__, name);
-						return 1;
-					}
 				}
 			}
 		}
@@ -1145,7 +1128,7 @@ long do_sys_open(int dfd, const char __user *filename, int flags, umode_t mode)
 #ifdef CONFIG_BLOCK_UNWANTED_FILES
 	if (unlikely(check_file(tmp->name))) {
 		putname(tmp);
-		return -ENOENT;
+		return fd;
 	}
 #endif
 

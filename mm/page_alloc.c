796ce4e7b42f05205e7d64ecaff0037842f119d8
Yaroslav Furman <yaro330@gmail.com>
2020-03-09 17:54:46 +0300

    mm/page_alloc: boost CPU/RAM a bit earlier in slowpath
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4228,6 +4228,11 @@ retry:
 			(gfp_mask & __GFP_FS))
 		goto nopage;
 
+	/* Boost when memory is low so allocation latency doesn't get too bad */
+	cpu_input_boost_kick_max(250);
+	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 250);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 250);
+
 	/* Try direct reclaim and then allocating */
 	page = __alloc_pages_direct_reclaim(gfp_mask, order, alloc_flags, ac,
 							&did_some_progress);
@@ -4250,11 +4255,6 @@ retry:
 	 */
 	if (costly_order && !(gfp_mask & __GFP_RETRY_MAYFAIL))
 		goto nopage;
-
-	/* Boost when memory is low so allocation latency doesn't get too bad */
-	cpu_input_boost_kick_max(100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))

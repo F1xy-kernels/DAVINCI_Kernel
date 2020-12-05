b990935eecc1e850022cc3eaf86f88a4003d1041
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-05 14:36:28 -0800

    dma-buf/sync_file: Remove debug names from sync data structures
    A lot of CPU time is wasted on allocating, populating, and copying
    debug names back and forth with userspace when they're not actually
    needed. We can't just remove the name buffers from the various sync data
    structures though because we must preserve ABI compatibility with
    userspace, but instead we can just pretend the name fields of the
    user-shared structs aren't there. This massively reduces the sizes of
    memory allocated for these data structures and the amount of data passed
    between userspace, improving graphics performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/dma-buf/sync_file.c
+++ drivers/dma-buf/sync_file.c
@@ -376,8 +376,10 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 	struct sync_file_info info;
 	struct sync_fence_info *fence_info = NULL;
 	struct dma_fence **fences;
-	size_t len, offset;
-	int num_fences, i;
+	__u32 size;
+	int num_fences, ret, i;
+	size_t offset;
+	size_t len;
 
 	arg += offsetof(typeof(info), status);
 	len = sizeof(info) - offsetof(typeof(info), status);
@@ -405,34 +407,29 @@ static long sync_file_ioctl_fence_info(struct sync_file *sync_file,
 	if (info.num_fences < num_fences)
 		return -EINVAL;
 
-	size = num_fences * sizeof(*fence_info);
-	fence_info = kzalloc(size, GFP_KERNEL);
+	offset = offsetof(typeof(*fence_info), status);
+	size = sizeof(*fence_info) - offset;
+	fence_info = kzalloc(size * num_fences, GFP_KERNEL);
 	if (!fence_info)
 		return -ENOMEM;
 
 	for (i = 0; i < num_fences; i++) {
-		struct {
-			__s32	status;
-			__u32	flags;
-			__u64	timestamp_ns;
-		} fence_info;
-		struct sync_fence_info *finfo = (void *)&fence_info - offset;
-		int status = sync_fill_fence_info(fences[i], finfo);
+		struct sync_fence_info *fence = (void *)&fence_info[i] - offset;
+		int status = sync_fill_fence_info(fences[i], fence);
 		u64 dest;
 
-		/* Don't leak kernel memory to userspace via finfo->flags */
-		finfo->flags = 0;
 		info.status = info.status <= 0 ? info.status : status;
-		dest = info.sync_fence_info + i * sizeof(*finfo) + offset;
-		if (copy_to_user(u64_to_user_ptr(dest), &fence_info,
-				 sizeof(fence_info)))
-			return -EFAULT;
+		dest = info.sync_fence_info + i * sizeof(*fence_info) + offset;
+		if (copy_to_user(u64_to_user_ptr(dest), &fence->status, size)) {
+			ret = -EFAULT;
+			goto out;
+		}
 	}
 
 no_fences:
 	info.num_fences = num_fences;
 
-	if (copy_to_user((void __user *)arg, &info, sizeof(info)))
+	if (copy_to_user((void __user *)arg, &info.status, len))
 		ret = -EFAULT;
 	else
 		ret = 0;
